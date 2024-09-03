23a4190265fe8557e04b31d4a7cef841006a857d
Rishikant Kumar Keshri <95926708+Rishi083@users.noreply.github.com>
2024-09-03 20:25:40 +0000

    variable

---
+++ basics_javascript/01_variable.js
@@ -0,0 +1,19 @@
+const accontId = 144553  //not change
+let accontEmail = "rishikeshri83@gmail.com" // less change
+var accontPassword = "12345" // not more use or not use 
+accontCity = "Banglore"
+
+//  accontId = 2  // not allowed
+console.log(accontId); 
+
+accontEmail = "rishi14@gmail.com"
+accontPassword = "234546"
+accontCity = "Jharkhand";
+let accountState;
+
+/*
+ prefer not to use var 
+ because of issue in block scope and funtional scope
+*/
+
+console.table([accontEmail, accontId, accontPassword ,accontCity, accountState])
