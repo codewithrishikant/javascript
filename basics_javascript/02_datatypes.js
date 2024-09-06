76b76525182c04a9bd51a841cf38b74c1b113e47
Rishikant Kumar Keshri <95926708+codewithrishikant@users.noreply.github.com>
2024-09-06 08:00:13 +0000

    Datatypes

---
+++ basics_javascript/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log(6+6)      // good code readability
+
+console.log("Hitesh")
+
+let name = "Rishikant" // string
+let age = 24 //number
+let isLoggedIn = false //boolean
+
+// data types
+/*
+      primative data type 
+
+   1 number => 2 to power 53
+   2 bigint
+   3 string => ""
+   4 boolen => True False
+   5 null => standalone value like empty value | empty space 
+   6 undefined => not given any value or data  like blank
+   7 symbol => unique value
+*/
+      //  object  
+
+        
+
+console.log(typeof "Rishi");  //string
+console.log( typeof 34)       //number
+console.log(typeof null)      //object
+console.log(typeof undefined) //undefined
+
+
