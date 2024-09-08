2030327baa2b749c22de299c5ca3321eff431cdd
Rishikant Kumar Keshri <95926708+codewithrishikant@users.noreply.github.com>
2024-09-08 22:09:25 +0000

    conversion

---
+++ basics_javascript/03_conversion.js
@@ -0,0 +1,58 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber1 = Number(score)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1)   //NaN
+
+// let score1 = null
+// console.log(typeof score1);
+// console.log(typeof(score1));
+
+// let valueInNumber2 = Number(score1)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2) // 0
+
+// let score2 = undefined
+// console.log(typeof score2);
+// console.log(typeof(score2));
+
+// let valueInNumber3 = Number(score2)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3)    // NaN
+
+/*   output  
+string
+string
+number
+NaN
+object
+object
+number
+0
+undefined
+undefined
+number
+NaN 
+*/
+
+/** "33" => 33
+ * "33abc => NaN"
+ * true => 1; false => 0
+ */
+
+// let isLoggedIn = "Rishi"
+// let boolenisLoggedIn  = Boolean(isLoggedIn)  //conversion
+// console.log(boolenisLoggedIn)
+
+/* 
+   1 = > true ; 0 => false
+   "" => false
+   "Rishi" => true
+*/
+
+let no = 33
+let stringNumber = String (no)
+console.log(stringNumber)
+console.log(typeof stringNumber)
