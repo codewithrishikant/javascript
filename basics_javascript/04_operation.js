c96a48cdd1061e8477d1b5d4b25e48839537319f
Rishikant Kumar Keshri <95926708+codewithrishikant@users.noreply.github.com>
2024-09-13 06:51:24 +0000

    operation

---
+++ basics_javascript/04_operation.js
@@ -0,0 +1,14 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(2+2);  //add
+console.log(2-2);  //subtract
+console.log(2*3);  //multiply
+console.log(2**3); //power
+console.log(2/3); // divide
+console.log(2%3);  //reminder
+
+let str1 = "Rishi"
+let str2 = "kant"
+let str3 = str1+str2
+console.log(str3)
