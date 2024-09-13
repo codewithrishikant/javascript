0a73389282e31a79774f6a316dc1bcfac2abd2c3
codewithrishikant <rishikeshri83@gmail.com>
2024-09-13 14:23:54 +0530

    code

--- basics_javascript/04_operation.js
+++ basics_javascript/04_operation.js
@@ -1,19 +1,28 @@
-let value = 3
-let negvalue = -value
-console.log(negvalue);
-console.log(2+2);  //add
-console.log(2-2);  //subtract
-console.log(2*3);  //multiply
-console.log(2**3); //power
-console.log(2/3); // divide
-console.log(2%3);  //reminder
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2);  //add
+// console.log(2-2);  //subtract
+// console.log(2*3);  //multiply
+// console.log(2**3); //power
+// console.log(2/3); // divide
+// console.log(2%3);  //reminder
 
-let str1 = "Rishi"
-let str2 = "kant"
-let str3 = str1+str2
-console.log(str3)
-
-
+// let str1 = "Rishi"
+// let str2 = "kant"
+// let str3 = str1+str2
+// console.log(str3)
 
 
-console.log("TESTINGGGGGGGGGGGGG")
+console.log("1" + 2); //12
+console.log("1"+"2"); //12
+
+
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "4"); //34
+console.log((3+4) *5 % 3); //2 good pratice of code 
+
+console.log(+true) // true = 1 
+console.log(true+1);// 2 
+console.log(+""); //0
+
