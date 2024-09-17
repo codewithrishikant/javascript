d72b3ed540d3d933fb2a38a1f345f787652b6bcf
codewithrishikant <rishikeshri83@gmail.com>
2024-09-18 00:26:39 +0530

    Zero_to_100

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,59 @@
+//fundamental of js part 1
+
+// let js = 'amazing';
+// if (js === 'amazing') alert("Javascript is a Good programing Language!");
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Rishi');
+// console.log(45);
+//********************
+
+//variable example
+// let firstname = 'Rishi';
+// let lastName = 'Keshri'
+// console.log(firstname + " " + lastName);
+//********************
+
+// Camel case example
+
+// let first = 'rishikant';
+// let middleName = 'Kumar'
+// let Last_name = 'Keshri'
+// let age$ = 24;
+
+// console.table([first, middleName, Last_name, age$]);
+// ******************************
+
+/**
+let 3year = 3;
+o/p - > starter\script.js:32
+let 3year = 3;
+    ^
+SyntaxError: Invalid or unexpected token
+
+let function = 24;
+
+o/p --> script.js:37
+let function = 24;
+    ^^^^^^^^
+SyntaxError: Unexpected token 'function'
+
+let pi = 3.1415; //define already
+let PI = 3.1415;
+ */
+//********************************/
+
+//Bad code pratice  ---> avoid this kind of mistake 
+let job1 = 'operation executive';
+let job2 = 'aenior system associate';
+
+//Good pratice of code   ----> always use this kind of code pratice 
+
+let myFirstJob = 'Operation executive';
+let myCurrentjob = 'Senior system associate';
+
+console.log('testing');
+
+
+
